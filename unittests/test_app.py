5d181b091a68d118d8ed65a036e335fe8218cf9f
Ravindra <raviinfo173@gmail.com>
2023-03-08 21:11:29 +0000

    modified workflow/add unittest

---
+++ unittests/test_app.py
@@ -0,0 +1,14 @@
+import unittest
+
+from unittest.mock import mock_open, patch
+
+
+class TestDeepvision(unittest.TestCase):
+    """ The function to test (would usually be loaded from a module outside this file).
+    """
+    def test_deepvision(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
