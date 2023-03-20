04809f4d4fd53c5395bc329411af21fafff70302
rainy <raiander@cisco.com>
2023-03-20 21:07:21 +0000

    container name changed

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 docker stop producer
 docker build --no-cache -t producer:latest .
 docker network create redisconnection
-docker run -p 6379:6379 --name redis --net redisconnection -d --rm redis
+docker run -p 6379:6379 --name redis_vision --net redisconnection -d --rm redis_vision
 docker run -p 5002:5001 --name producer --net redisconnection -v $(pwd)/:/app -itd --rm producer:latest
-docker exec -it producer bash -c "python3 producer.py --verbose True -u redis://redis:6379 data/race.mp4 & python3 server.py -u redis://redis:6379"
+docker exec -it producer bash -c "python3 producer.py --verbose True -u redis://redis_vision:6379 data/race.mp4 & python3 server.py -u redis://redis_vision:6379"
