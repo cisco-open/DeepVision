d7efc8e99d24869521fedfeb5435d54551fdb571
hugo <hlatapie@cisco.com>
2023-05-28 20:33:35 -0700

    ONA is now hooked in to the cognitive synergy engine

--- cognitivesynergy/CognitiveSynergyAgent.py
+++ cognitivesynergy/CognitiveSynergyAgent.py
@@ -2,6 +2,7 @@ import redis
 import json
 import threading
 import time
+from ONA.DockerInteractor import DockerInteractor
 
 class CognitiveSynergyAgent:
     def __init__(self, agent_type, config):
@@ -9,19 +10,23 @@ class CognitiveSynergyAgent:
         self.config = config
         self.is_running = False
         self._thread = None
+        self.ona = DockerInteractor(['docker', 'exec', '-i', 'ONA', '/app/NAR', 'shell'])
 
         # Set up Redis client
         #self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
     def _run(self):
+        iterations = 0
         print(f"Agent of type {self.agent_type} is starting...")
 
         while self.is_running:
             # Process data and publish results to Redis
             data = {}  # Your data here
             #self.redis_client.publish('channel_name', json.dumps(data))
-
-            # Let's pause for a moment to simulate processing time
+            iterations = iterations + 1
+            print(f"Agent of type {self.agent_type} is running...iteration {iterations}")
+            response = self.ona.execute_command("<cat --> furry_animal>.\n<cat --> furry_animal>?\n0\n", "done with 0 additional inference steps")
+            print(f"Response: {response}")
             time.sleep(1)
 
         print(f"Agent of type {self.agent_type} has stopped.")
