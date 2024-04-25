b5f04676b0dd07d485923c7afbfd649b15811872
Ubuntu <ubuntu@devbackend.lxd>
2024-04-25 20:44:32 +0000

    some minor updates

--- Ethosight/bin/runserver.py
+++ Ethosight/bin/runserver.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 # Copyright 2022 Cisco Systems, Inc. and its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 # SPDX-License-Identifier: Apache-2.0
-
-#!/usr/bin/env python3
 import click
 import os
 import uvicorn
