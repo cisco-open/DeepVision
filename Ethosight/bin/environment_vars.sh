cc56e6e9969c8645b98a89c8ce071a2dea42aee7
Hugo <hlatapie@cisco.com>
2024-05-01 03:27:43 +0000

    added support for django

---
+++ Ethosight/bin/environment_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export EthosightBackend="client"
+export EthosightBackendURL="http://localhost:80"
+export DjangoEthosightAppBaseDir="/home/ubuntu/lxdshared/DeepVision/Ethosight/website/EthosightAppBasedir"
+export EthosightYAMLDirectory="/home/ubuntu/lxdshared/DeepVision/Ethosight/configs"
+export ETHOSIGHT_APP_BASEDIR="/home/ubuntu/lxdshared/DeepVision/Ethosight/website/EthosightAppBasedir"
+export EMAIL_HOST="email-smtp.us-east-1.amazonaws.com"
+export EMAIL_PORT="587"
+export EMAIL_USE_TLS="True"
+export EMAIL_USE_SSL="False"
+export EMAIL_HOST_USER="vault_email_host_user"
+export EMAIL_HOST_PASSWORD="vault_email_host_password"
+
